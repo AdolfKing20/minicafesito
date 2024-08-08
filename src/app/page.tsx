fb6c59d666c73e403f355421dcd73a5c6aae395a
AdolfKing <rodrigogfxs@gmail.com>
2024-08-07 21:39:53 -0300

    doname algo rata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FormMp from "@/components/FormMp"
 export default function Home() {
   return (
    <>
+   <h1 className="text-3xl font-bold text-center uppercase mb-4">Doname algo rata</h1>
    <FormMp />
    </>
   );
